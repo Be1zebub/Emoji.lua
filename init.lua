19430f5962ecb3bf25392fd9aaf1f2c69730be49
Beelzebub <enerrr.wsed@gmail.com>
2022-10-31 09:24:57 +0300

    v2.1

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 local Emoji = {
-	_VERSION = 2.0,
+	_VERSION = 2.1,
 	_URL 	 = "https://github.com/Be1zebub/Emoji.lua",
 	_LICENSE = [[
 		MIT LICENSE
@@ -23,23 +23,57 @@ local Emoji = {
 	]]
 }
 
-file.CreateDir("emoji.lua")
-file.CreateDir("emoji.lua/db")
-file.CreateDir("emoji.lua/assets")
+local fs, httpGET, jsonDecode
+
+if gmod then
+	fs, httpGET, jsonDecode = file, http.Fetch, util.JSONToTable
+elseif process and process.argv and (function()
+	for _, arg in pairs(process.argv) do
+		if arg:find("luvit", 1, true) then
+			return true
+		end
+	end
+
+	return false
+end)() then
+	fs = {
+		Write 		= require("fs").writeFileSync,
+		Read 		= require("fs").readFileSync,
+		CreateDir 	= require("fs").mkdirSync
+	}
+
+	local request = require("coro-http").request
+
+	httpGET = function(url, cback)
+		local success, res, response = pcall(request, "GET", url)
+		if success then
+			cback(response)
+		end
+	end
+
+	jsonDecode = require("json").decode
+end
+
+fs.CreateDir("emoji.lua")
+fs.CreateDir("emoji.lua/db")
+fs.CreateDir("emoji.lua/assets")
 
 local function ReadOrFetch(path, url, cback)
-	if file.Exists(path, "DATA") then
-		cback(file.Read(path, "DATA"))
+	if fs.Exists(path, "DATA") then
+		cback(fs.Read(path, "DATA"))
+		return true
 	else
-		http.Fetch(url, function(reponse)
-			file.Write(path, response)
-			cback(reponse)
+		httpGET(url, function(response)
+			fs.Write(path, response)
+			cback(response)
 		end)
 	end
 end
 
 setmetatable(Emoji, {
-	__call = function(self, repo, dbName, branch)
+	__call = function(self, repo, dbName, defaultAssets, branch)
+		local running = coroutine.running()
+
 		local instance = setmetatable({
 			dbName = dbName,
 			db = {},
@@ -48,14 +82,15 @@ setmetatable(Emoji, {
 			raw = "https://raw.githubusercontent.com/".. repo .."/".. (branch or "master") .."/"
 		}, self)
 
-		ReadOrFetch("emoji.lua/db/".. dbName ..".json", instance.raw .."db/".. dbName ..".json", function(json)
-			local db = util.JSONToTable(json)
+		if ReadOrFetch("emoji.lua/db/".. dbName ..".json", instance.raw .."db/".. dbName ..".json", function(json)
+			local db = jsonDecode(json)
 
 			instance.assets = db.assets
 			instance.db = db.emoji
+			instance.defaultAssets = defaultAssets or db.defaultAssets or instance.assets[1]
 
 			for _, dir in ipairs(instance.assets) do
-				file.CreateDir("emoji.lua/assets/".. dir)
+				fs.CreateDir("emoji.lua/assets/".. dir)
 			end
 
 			for _, data in ipairs(instance.db) do
@@ -63,9 +98,13 @@ setmetatable(Emoji, {
 					instance.aliases[alias] = data.uid
 				end
 			end
-		end)
 
-		return instance
+			coroutine.resume(running, instance)
+		end) then
+			return instance
+		end
+
+		return coroutine.yield()
 	end
 })
 
@@ -88,8 +127,10 @@ function Emote:Download(assetPack)
 	return self.parent:Download(self.uid, assetPack)
 end
 
-function Emote:Material(assetPack, flags)
-	return self.parent:Material(self.uid, assetPack, flags)
+if gmod then
+	function Emote:Material(assetPack, flags)
+		return self.parent:Material(self.uid, assetPack, flags)
+	end
 end
 
 function Emote:__tostring()
@@ -97,6 +138,8 @@ function Emote:__tostring()
 end
 
 --—————————————————— Emoji meta ——————————————————--
+
+Emoji.__index = Emoji
 
 function Emoji:Lookup(alias)
 	local emote = self.aliases[alias:gsub(":", "")]
@@ -110,20 +153,20 @@ end
 
 -- ":airplane:" > Emote("emoji", "2708-fe0f")
 
-function Emoji:Search(query, isPattern)
+function Emoji:Search(query, isPattern, storage)
 	isPattern = isPattern or false
 
-	local out = {}
+	storage = storage or {}
 
 	for _, data in ipairs(self.db) do
 		for _, alias in ipairs(data.aliases) do
 			if alias:find(query, 1, isPattern) then
-				out[#out + 1] = self:Lookup(alias)
+				storage[#storage + 1] = self:Lookup(alias)
 			end
 		end
 	end
 
-	return out
+	return storage
 end
 
 -- "thumb" > {Emote("emoji", "1f44d"), Emote("emoji", "1f44e")}
@@ -160,7 +203,7 @@ end
 function Emoji:Path(uid, assetPack)
 	assetPack = isstring(assetPack) and assetPack or (
 		self.assets[assetPack] or
-		self.assets[1]
+		self.defaultAssets
 	)
 
 	return "assets/".. assetPack .."/".. uid ..".png"
@@ -177,7 +220,7 @@ end
 function Emoji:Download(uid, assetPack)
 	local running = coroutine.running()
 
-	http.Fetch(self:URL(uid, assetPack), function(raw, _, _, code)
+	httpGET(self:URL(uid, assetPack), function(raw, _, _, code)
 		if code ~= 200 or raw:find("<!DOCTYPE HTML>", 1, true) then
 			coroutine.resume(running)
 		else
@@ -196,15 +239,17 @@ coroutine.wrap(function()
 end)()
 ]]--
 
-function Emoji:Material(uid, assetPack, flags)
-	local running = coroutine.running()
-	local path = self:Path(uid, assetPack)
+if gmod then
+	function Emoji:Material(uid, assetPack, flags)
+		local running = coroutine.running()
+		local path = self:Path(uid, assetPack)
 
-	ReadOrFetch(path, self:URL(uid, assetPack), function()
-		coroutine.resume(running, Material("data/".. path, flags))
-	end)
+		ReadOrFetch(path, self:URL(uid, assetPack), function()
+			coroutine.resume(running, Material("data/".. path, flags or "smooth mips"))
+		end)
 
-	return coroutine.yield()
+		return coroutine.yield()
+	end
 end
 
 --[[
@@ -242,77 +287,145 @@ end
 -- "✈️" > 2708-fe0f
 -- "👍", 3 > 1f44d-1f3fd
 
+--—————————————————— Collection ——————————————————--
+
+local Collection = {}
+
+Collection.__index = Collection
+
+setmetatable(Collection, {
+	__call = function(self, ...)
+		local instance = setmetatable({list = {}}, self)
+
+		if ... then
+			for _, emotes in ipairs({...}) do
+				instance:Add(emotes)
+			end
+		end
+
+		return instance
+	end
+})
+
+function Collection:Add(emotes)
+	self.list[#self.list + 1] = emotes
+	return self
+end
+
+function Collection:Lookup(alias)
+	for _, emotes in ipairs(self.list) do
+		local emote = emotes:Lookup(alias)
+		if emote then
+			return emote
+		end
+	end
+end
+
+function Collection:Search(query, isPattern, storage)
+	storage = storage or {}
+
+	for _, emotes in ipairs(self.list) do
+		emotes:Search(query, isPattern, storage)
+	end
+
+	return storage
+end
+
+function Collection:Parse(text)
+	return self.list[1].Parse(self, text)
+end
+
+function Emoji:Collection(...)
+	return Collection(...)
+end
+
 --—————————————————— Debug menu ——————————————————--
 
 function Emoji:Debug()
-	local emoji = self("Be1zebub/Emoji.lua", "emoji")
+	surface.CreateFont("Emoji.lua", {
+		size = 24,
+		font = "Roboto",
+		extended = true,
+		antialias = true
+	})
 
-	local menu = vgui.Create("DFrame")
-	menu:SetSize(400, 240)
-	menu:Center()
-	menu:MakePopup()
-	menu:SetTitle("Emoji.lua v".. (math.floor(self._VERSION) == self._VERSION and self._VERSION ..".0" or self._VERSION))
-	menu:DockPadding(11, 26 + 11, 11, 11)
-	menu.Paint = function(me, w, h)
-		surface.SetDrawColor(54, 57, 63)
-		surface.DrawRect(0, 0, w, h)
+	coroutine.wrap(function()
+		local emojis = Emoji:Collection()
+		:Add(Emoji("Be1zebub/Emoji.lua", "emoji", "emoji/noto"))
+		:Add(Emoji("Be1zebub/Emoji.lua", "elite-pepe"))
+		:Add(Emoji("Be1zebub/Emoji.lua", "elite-gorgeous"))
+		:Add(Emoji("Be1zebub/Emoji.lua", "elite-roflan"))
 
-		surface.SetDrawColor(32, 34, 37)
-		surface.DrawRect(0, 0, w, 26)
-	end
+		local menu = vgui.Create("DFrame")
+		menu:SetSize(440, 300)
+		menu:Center()
+		menu:MakePopup()
+		menu:SetTitle("Emoji.lua v".. (math.floor(self._VERSION) == self._VERSION and self._VERSION ..".0" or self._VERSION))
+		menu:DockPadding(11, 26 + 11, 11, 11)
+		menu.Paint = function(me, w, h)
+			surface.SetDrawColor(54, 57, 63)
+			surface.DrawRect(0, 0, w, h)
 
-	local scroll = menu:Add("DScrollPanel")
-	scroll:Dock(FILL)
-	scroll.AddText = function(me, text)
-		Msg(string.rep("\n", 2))
-		print("\n> ".. text)
-		PrintTable(emoji:Parse(text))
+			surface.SetDrawColor(32, 34, 37)
+			surface.DrawRect(0, 0, w, 26)
+		end
 
-		local msg = me:Add("EditablePanel")
-		msg:Dock(TOP)
-		msg:DockMargin(0, 8, 0, 0)
+		local scroll = menu:Add("DScrollPanel")
+		scroll:Dock(FILL)
+		scroll.AddText = function(me, text)
+			--Msg(string.rep("\n", 2))
+			--print("\n> ".. text)
+			--PrintTable(emojis:Parse(text))
 
-		coroutine.wrap(function()
-			for _, part in ipairs(emoji:Parse(text)) do
-				if part.IsEmoji then
-					if inc_ui then
-						local img = msg:Add("inc_ui/material")
-						img:Dock(LEFT)
-						img:SetSize(22, 22)
-						img:SetIcon(part:URL("emoji/twitter"))
+			local msg = me:Add("EditablePanel")
+			msg:Dock(TOP)
+			msg:DockMargin(0, 8, 0, 0)
+
+			coroutine.wrap(function()
+				for _, part in ipairs(emojis:Parse(text)) do
+					if part.IsEmoji then
+						if inc_ui then
+							local img = msg:Add("inc_ui/material")
+							img:Dock(LEFT)
+							img:SetWide(msg:GetTall())
+							img:SetIcon(part:URL())
+						else
+							local img = msg:Add("DImage")
+							img:Dock(LEFT)
+							img:SetWide(msg:GetTall())
+							img:SetMaterial(part:Material(nil, "smooth mips"))
+						end
 					else
-						local img = msg:Add("DImage")
-						img:Dock(LEFT)
-						img:SetSize(22, 22)
-						img:SetMaterial(part:Material("emoji/twitter", "smooth mips"))
+						local txt = msg:Add("DLabel")
+						txt:Dock(LEFT)
+						txt:SetText(part)
+						txt:SetFont("Emoji.lua")
+						txt:SizeToContents()
 					end
-				else
-					local txt = msg:Add("DLabel")
-					txt:Dock(LEFT)
-					txt:SetText(part)
-					txt:SizeToContents()
 				end
-			end
-		end)()
-	end
+			end)()
+		end
 
-	local entry = menu:Add("DTextEntry")
-	entry:Dock(BOTTOM)
-	entry:SetTall(32)
-	entry:SetTextColor(Color(235, 235, 235))
-	entry:SetCursorColor(Color(220, 220, 220))
-	entry.OnEnter = function(me)
-		scroll:AddText(me:GetValue())
-	end
-	entry.Paint = function(me, w, h)
-		draw.RoundedBox(6, 0, 0, w, h, Color(64, 68, 75))
-		me:DrawTextEntryText(me:GetTextColor(), me:GetHighlightColor(), me:GetCursorColor())
-	end
+		local entry = menu:Add("DTextEntry")
+		entry:Dock(BOTTOM)
+		entry:SetTall(32)
+		entry:SetTextColor(Color(235, 235, 235))
+		entry:SetCursorColor(Color(220, 220, 220))
+		entry.OnEnter = function(me)
+			scroll:AddText(me:GetValue())
+		end
+		entry.Paint = function(me, w, h)
+			draw.RoundedBox(6, 0, 0, w, h, Color(64, 68, 75))
+			me:DrawTextEntryText(me:GetTextColor(), me:GetHighlightColor(), me:GetCursorColor())
+		end
 
-	scroll:AddText(":wave: world! I love :new_moon_with_face:")
-	scroll:AddText("i have :medal_sports:, because im cool!")
+		scroll:AddText(":wtf: Gorgeous freeman :pleasure: emote pack :not_bad:")
+		scroll:AddText(":wave: world! I love :new_moon_with_face:")
+		scroll:AddText("i have :medal_sports:, because im cool :owo:")
+		scroll:AddText(":hmm: hmm, seems good :very_dovolen:")
+	end)()
 end
 
-Emoji:Debug()
+--Emoji:Debug()
 
 return Emoji
